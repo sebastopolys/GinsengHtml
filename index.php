44435f328a08f08e198a01484a2a28fba78abff8
Seba Rossi <sebastopolys@gmail.com>
2024-07-08 20:16:23 -0300

    upate autoloader

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+namespace ginsenghtmlengine;
+
+ini_set( 'display_errors', 1 );
+ini_set( 'display_startup_errors', 1 );
+error_reporting( E_ALL );
+
+ if( file_exists( __DIR__ ).'/vendor/autoload.php' ){
+        require  __DIR__ .'/vendor/autoload.php';
+    } else {
+        die( '<pre>ERROR: cannot load autoloader file</pre>' );
+    }
+ 
+// Start the thing
+
